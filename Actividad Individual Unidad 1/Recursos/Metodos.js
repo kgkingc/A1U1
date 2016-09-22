336c57ac4a0b7173957257d752e037f4ffee1a87
kgkingc <kkingc@misena.edu.co>
2016-09-22 17:18:12 -0500

    Add files via upload

---
+++ Actividad Individual Unidad 1/Recursos/Metodos.js
@@ -0,0 +1,13 @@
+function Welcome() {
+    var panel1 = document.getElementById('construction');
+    var panel2 = document.getElementById('welcome');
+    panel1.style.display = 'none';
+    panel2.style.display = '';
+}
+
+function UnderConstruction() {
+var panel1 = document.getElementById('construction');
+    var panel2 = document.getElementById('welcome');
+    panel1.style.display = '';
+    panel2.style.display = 'none';
+}
